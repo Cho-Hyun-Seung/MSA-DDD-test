9d064461e7e785a820b38e0405e843a90edbb7a7
Cho-Hyun-Seung <toki0327@naver.com>
2026-03-03 01:18:26 +0900

    fix: fixed Feign Client

--- order-service/src/main/java/com/example/orderservice/infrastructure/client/ProductFeignClient.java
+++ order-service/src/main/java/com/example/orderservice/infrastructure/client/ProductFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "product-service", url = "http://product-service:8080")
+@FeignClient(name = "product-service", url = "http://product-service:8080/api")
 public interface ProductFeignClient {
 
     @GetMapping("/product/{id}")
